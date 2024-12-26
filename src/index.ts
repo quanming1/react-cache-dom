9bffa5f1fea5601ce2df77ffb10638d4792671a6
jiang_quanming <jiangquanming@datagrand.com>
2024-12-26 12:10:43 +0800

    update: updated type exports

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { CacheDom, type CacheDomProps } from "./components/CacheDom/CacheDom";
 export {
   CacheGroup,
   type CacheGroupProps,
+  type CacheGroupRef,
 } from "./components/CacheDom/CacheGroup";
